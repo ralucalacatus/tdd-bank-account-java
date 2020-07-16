900fc40919ada5fccafbcc84f7fe9d9075d1d058
Raluca Lacatus <raluca.lacatusM@yahoo.com>
2020-07-16 16:52:47 +0300

    tdd test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,12 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
+    public int balance() {
+        return balance;
+    }
+
+    public void deposit(int depositedAmount) {
+        balance += depositedAmount;
+    }
 }
